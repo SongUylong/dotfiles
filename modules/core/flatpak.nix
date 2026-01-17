83161231e32de63fbadb417cd9a1d3d1e86629f3
uylong <songuylong24@gmail.com>
2026-01-17 21:03:38 +0700

    add flatpak

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -3,17 +3,5 @@
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
   services.flatpak = {
-    enable = false;
-    packages = [ "com.github.tchx84.Flatseal" ];
-    overrides = {
-      global = {
-        # Force Wayland by default
-        Context.sockets = [
-          "wayland"
-          "!x11"
-          "!fallback-x11"
-        ];
-      };
-    };
-  };
+    enable = true;
 }
